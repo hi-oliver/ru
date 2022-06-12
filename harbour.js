21bc824c7faa94cdbf42d1b99489bb4a9756065f
SIZOV_A <106088206+aetross@users.noreply.github.com>
2022-06-12 20:49:52 +0300

    adq. v1.0

---
+++ harbour.js
@@ -0,0 +1,15 @@
+function myFunction() {
+  var wind =
+  document.getElementById("piPie");
+  wind.classList.toggle("show");
+}
+function myFunctions() {
+  var razer =
+  document.getElementById("colPie");
+  razer.classList.toggle("show");
+}
+function myFunctionses() {
+  var honey =
+  document.getElementById("goal");
+  honey.classList.toggle("show");
+}
